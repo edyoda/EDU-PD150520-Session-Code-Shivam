f875b99051b2e09c159267285e31f3cced118d53
jindalg <jindalshivam65@gmail.com>
2020-11-05 11:24:08 +0530

    Django generic view

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import index, product_detail, products, orders, add_product, login
+from django.views import generic
+
+from blog.views import (
+    index, product_detail, products, orders, add_product, Login, AddProductView,
+    ProductListView, ProductDetailView
+)
 
 urlpatterns = [
-    path('login', login),
+    # path('login', login),
+    path('base', generic.TemplateView.as_view(template_name='base.html')),
+    path('login', Login.as_view()),
     path('index/<int:person_id>', index),
-    path('product/<int:product_id>', product_detail, name='product_detail'),
-    path('products', products),
-    path('add_product', add_product),
-    path('orders/<int:user_id>', orders)
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('products', ProductListView.as_view()),
+    # path('add_product', add_product),
+    path('add_product', AddProductView.as_view()),
+    path('add_product_duplicate', generic.RedirectView.as_view(url='/login')),
+    path('orders/<int:user_id>', orders),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+import datetime
+from django.views import generic
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views import View
 # Create your views here.
 from blog.models import User, Product, Order
 from blog.forms import ProductForm, LoginForm
@@ -7,6 +10,21 @@ from blog.forms import ProductForm, LoginForm
 def index(request, person_id):
 	person = User.objects.filter(id=person_id).first()
 	return HttpResponse('hello we are live now.' + person.name if person else 'nothing')
+
+
+class AddProductView(View):
+	def get(self, request):
+		form = ProductForm()
+		return render(request, 'add_product.html', {'form': form})
+	
+	def post(self, request):
+		form = ProductForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/products')
+		else:
+			return render(request, 'add_product.html', {'form': form})
+
 
 def add_product(request):
 	if request.method == 'GET':
@@ -33,9 +51,45 @@ def login(request):
 			return render(request, 'login.html', {'form': form})
 
 
+class Login(View):
+	def get(self, request):
+		form = LoginForm()
+		return render(request, 'login.html', {'form': form})
+	
+	def post(self, request):
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			return redirect('/products')
+		else:
+			return render(request, 'login.html', {'form': form})
+
+
 def product_detail(request, product_id):
 	product = Product.objects.filter(id=product_id).first()
 	return render(request, 'product_detail.html', {'product': product})
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'my_product'
+	
+    # def get_object(self):
+    #     obj = self.get_queryset().filter(id=1).first()
+    #     print(obj)
+    #     return obj
+
+
+class ProductListView(generic.ListView):
+	# model = Product
+	template_name = 'products.html'
+	context_object_name = 'products'
+	queryset = Product.objects.filter()
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['now'] = datetime.datetime.now()
+		return context
 
 
 def products(request):
